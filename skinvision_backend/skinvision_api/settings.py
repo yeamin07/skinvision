15992f50f3e0ff9cae7e30f5583b83fe2dd14c15
yeamin <yeaminr07@gmail.com>
2026-08-14 22:14:14 +0600

    Configure production settings for Render deployment

--- skinvision_backend/skinvision_api/settings.py
+++ skinvision_backend/skinvision_api/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-v_swb01@nd0s!kqsc*n2(zx5_o*wcx)=cx$u^_!i&z*z22^l0-'
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key-change-in-render')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-print(SECRET_KEY)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
