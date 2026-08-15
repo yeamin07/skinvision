60345d67381489e8d001d293ea1bcc65d1c4d588
yeamin <yeaminr07@gmail.com>
2026-08-16 02:13:34 +0600

    updated requirements.txt

--- skinvision_backend/build.sh
+++ skinvision_backend/build.sh
@@ -1,7 +1,7 @@
 set -o errexit
 
-pip install -r skinvision_backend/requirements.txt
-
+pip install --upgrade pip
+pip install --no-cache-dir -r skinvision_backend/requirements.txt
 python manage.py collectstatic --no-input
 
 # python manage.py migrate
