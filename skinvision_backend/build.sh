3c10e30bfa938f1b8dafe4c4f1da1816fd232b59
yeamin <yeaminr07@gmail.com>
2026-08-16 02:19:06 +0600

    updated requirements.txt

--- skinvision_backend/build.sh
+++ skinvision_backend/build.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 set -o errexit
 
 pip install --upgrade pip
 pip install --no-cache-dir -r skinvision_backend/requirements.txt
+
+# Change to the Django project directory
+cd skinvision_backend
+
 python manage.py collectstatic --no-input
+# python manage.py migrate
 
 # python manage.py migrate
