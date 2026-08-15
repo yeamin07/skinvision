37468a4e3c7efe000fd7b317aeea0519f47bf69d
yeamin <yeaminr07@gmail.com>
2026-08-16 02:23:19 +0600

    updated requirements.txt

--- skinvision_backend/skinvision_api/deployment_settings.py
+++ skinvision_backend/skinvision_api/deployment_settings.py
@@ -1,7 +1,7 @@
 import os 
 import dj_database_url
-from settings import *
-from settings import BASE_DIR
+from .settings import *
+from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
