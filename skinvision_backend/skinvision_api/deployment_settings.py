96bb5ef3c8bb6580401fb49c512e3ec983433ae2
yeamin <yeaminr07@gmail.com>
2026-08-17 01:15:36 +0600

    ...

--- skinvision_backend/skinvision_api/deployment_settings.py
+++ skinvision_backend/skinvision_api/deployment_settings.py
@@ -22,7 +22,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://skinvision-frontend.onrender.com/",
+    "'https://skinvision-frontend.railway.app'",
 ]
 
 STORAGES = {
--- skinvision_backend/skinvision_api/settings.py
+++ skinvision_backend/skinvision_api/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'l!=wu*g853tsnvm_(kopyd4bj&oj1=_xn+3n28x5xzgv$u*8a2'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
